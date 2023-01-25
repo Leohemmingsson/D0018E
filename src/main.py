7197e684bd4e5a8db5d3970ff46cde09a6785e33
Epos95 <39768937+Epos95@users.noreply.github.com>
2023-01-25 16:30:45 +0100

    basic implementation
    Basic stuff works, still need to add database connection

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 
 app = Flask(__name__)
@@ -10,6 +10,45 @@ def hello_world():
         "index.html",
         variable="oooo",
     )
+
+
+@app.route("/admin", methods=["GET"])
+def admin():
+    # Only give access to this page if the cookie matches a admin
+    verification_cookie = request.cookies.get("verification")
+
+    # No cookie, user definetly unauthorized.
+    if not verification_cookie:
+        return "Access denied!"
+
+    # Check if the verification cookie matches any admin in the database
+
+    pass
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html", error="jdafg")
+
+    elif request.method == "POST":
+        username = request.form["uname"]
+        password = request.form["psw"]
+
+        print(f"Tried to login as: {username} with password {password}")
+
+        # Check if password and username is in the users table.
+        # Redirect based on the response:
+        # If the found user is a normal user, redirect to index.html with a logged in cookie
+        # res = make_response(render_template("index.html"))
+        # res.set_cookie("verification", user_id_from_sql_query)
+        # if the user is a admin, redirect to admin_page.html with a logged in cookie
+        # if none, redirect to login.html with a error
+        #
+        # The logged in cookie's content should be the users id. This is insecure but easily changeable
+        # and shouldnt matter too much since its just a demo.
+
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
