61dd902f543943bd9c31e17c9d9f88c24a693f91
epos95 <maxagnesund95@gmail.com>
2023-02-01 00:53:34 +0100

    Revert "added simple user view and foundation for user management"
    This reverts commit 633fbc738c41d0d814316e7674b7a7380f125bd3.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,14 @@
 from flask import Flask, render_template, request, make_response, g, redirect, url_for
 from dotenv import load_dotenv
-import os
+import os 
 import mysql.connector
-
-# Import logger object and set it up to log to stdout and a file
-from logger import logger
-
-log = logger(logger.STDOUT | logger.FILE)
-
-# This method should be used in the future if we split into multiple python files
-# In that case we can just do `from main import l; l().log("text")`
-def l():
-    return log
 
 
 app = Flask(__name__)
 
 
 def get_db():
-    if "db" not in g:
+    if 'db' not in g:
         load_dotenv()
         mydb = mysql.connector.connect(
             host=os.getenv("SERVER_IP"),
@@ -31,14 +21,12 @@ def get_db():
 
     return g.db
 
-
 @app.teardown_appcontext
 def close_db(exception):
-    db = g.pop("db", None)
+    db = g.pop('db', None)
 
     if db is not None:
         db.close()
-
 
 @app.route("/")
 def index():
@@ -75,9 +63,7 @@ def login():
         cursor = db.cursor()
 
         # Check if password and username is in the users table.
-        cursor.execute(
-            f"SELECT * FROM User WHERE username = '{username}' and password = '{password}'"
-        )
+        cursor.execute(f"SELECT * FROM User WHERE username = \'{username}\' and password = \'{password}\'")
         result = cursor.fetchall()
 
         # The query returned results and, therefore, user(s)
@@ -88,15 +74,16 @@ def login():
             username: str = result[0][1]
             user_type: str = result[0][5]
 
+            
             # TODO: Add as proper logging later
-            log.log(f"{username} ({uid}) logged in as {user_type}")
-
+            print(f"{username} ({uid}) logged in as {user_type}")
+            
             # Redirect based on user type
             if user_type == "admin":
-                log.log("redirecting to admin.html")
+                print("redirecting to admin.html")
                 res = make_response(redirect(url_for("admin")))
             else:
-                log.log("redirecting to index.html")
+                print("redirecting to index.html")
                 res = make_response(redirect(url_for("index")))
 
             res.set_cookie("verification", str(uid))
@@ -106,9 +93,8 @@ def login():
             # TODO: Maybe a fail2ban system in the future.
 
             # Invalid login! Return a error and log the event.
-            log.log(f"Someone tried to log in as {username} with password {password}")
+            print(f"Someone tried to log in as {username} with password {password}")
             return render_template("login.html", error="Account not found!")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
