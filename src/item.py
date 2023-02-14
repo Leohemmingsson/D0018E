c02a5403e75b8b8d613f4a3b8e84b629ab18be4f
Leo <leohemmingsson@gmail.com>
2023-02-14 16:40:06 +0100

    fixed item constructor

--- src/item.py
+++ src/item.py
@@ -1,9 +1,18 @@
 class Item:
-    def __init__(self, product):
-        self.id = product[0]
-        self.description = product[2]
-        self.name = product[1]
-        self.quantity = product[3]
-        self.price = product[4]
-        self.image = product[5]
-        self.href = f"/product/{product[0]}"
+    def __init__(self, product=None, **kwargs):
+        if len(kwargs) == 0:
+            self.id = product[0]
+            self.description = product[2]
+            self.name = product[1]
+            self.quantity = product[3]
+            self.price = product[4]
+            self.image = product[5]
+            self.href = f"/product/{product[0]}"
+        else:
+            self.id = kwargs["id"]
+            self.description = kwargs["description"]
+            self.name = kwargs["name"]
+            self.quantity = kwargs["quantity"]
+            self.price = kwargs["price"]
+            self.image = kwargs["image"]
+            self.href = f"/product/{kwargs['id']}"
