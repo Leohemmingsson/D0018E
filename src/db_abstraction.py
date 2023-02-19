549c24828d92e2b7f66c425b85cc67c85b89bbcd
Leo <leohemmingsson@gmail.com>
2023-02-19 16:41:23 +0100

    added cart commands

--- src/db_abstraction.py
+++ src/db_abstraction.py
@@ -63,35 +63,120 @@ class DB:
         self.mydb.commit()
 
     def set_user_type(self, user_id, new_type):
-        self.cursor.execute(
-            f"UPDATE User SET type = \"{new_type}\" where id = {user_id}"
-        )
+        self.cursor.execute(f'UPDATE User SET type = "{new_type}" where id = {user_id}')
         self.mydb.commit()
 
     def delete_user_by_id(self, user_id):
-        self.cursor.execute(f"DELETE FROM User WHERE id = {json['id']}")
+        self.cursor.execute(f"DELETE FROM User WHERE id = {user_id['id']}")
         self.mydb.commit()
 
     def is_admin(self, cookie):
         if not cookie:
             return False
 
-        self.cursor.execute(f'SELECT * FROM User WHERE id = {cookie} and type = "admin"')
+        self.cursor.execute(
+            f'SELECT * FROM User WHERE id = {cookie} and type = "admin"'
+        )
         return len(self.cursor.fetchall()) > 0
 
     def get_users(self):
         self.cursor.execute("select * from User")
         return self.cursor.fetchall()
 
+    ### STUFF WITH CART ###
+
+    def get_cart(self, user_id):
+        cart_id = self.__get_active_cart_id(user_id)
+        sql = "SELECT Item.*, ItemGroup.quantity FROM Item LEFT JOIN ItemGroup ON Item.id = ItemGroup.item_id WHERE ItemGroup.order_id = %s"
+        val = (cart_id,)
+        self.cursor.execute(sql, val)
+        return self.cursor.fetchall()
+
+    def add_to_cart(self, user_id, item_id):
+        """
+        Add a item to the cart of the user
+        If item in cart, increase quantity
+        """
+        cart_id = self.__get_active_cart_id(user_id)
+
+        if not self.__is_item_in_cart(user_id, item_id, cart_id):
+            self.__add_item_to_cart(cart_id, item_id)
+            return
+        self.__add_quant_cart_item(cart_id, item_id)
+
+    def remove_from_cart(self, user_id, item_id):
+        """
+        Subtract one from the quantity of the item in the cart
+        if only 1 left, remove item from cart
+        """
+        cart_id = self.__get_active_cart_id(user_id)
+
+        if self.__quantity_in_cart(user_id, item_id)[0] > 1:
+            self.__remove_one_from_item(cart_id, item_id)
+            return
+
+        sql = "DELETE FROM ItemGroup WHERE order_id = %s and item_id = %s"
+        val = (cart_id, item_id)
+        self.cursor.execute(sql, val)
+        self.mydb.commit()
+
+    def __quantity_in_cart(self, user_id, item_id):
+        cart_id = self.__get_active_cart_id(user_id)
+
+        sql = "SELECT quantity FROM ItemGroup WHERE order_id = %s and item_id = %s"
+        val = (cart_id, item_id)
+        self.cursor.execute(sql, val)
+        return self.cursor.fetchone()
+
+    def __remove_one_from_item(self, cart_id, item_id):
+        sql = "UPDATE ItemGroup SET quantity = quantity - 1 WHERE order_id = %s and item_id = %s"
+        val = (cart_id, item_id)
+        self.cursor.execute(sql, val)
+        self.mydb.commit()
+
+    def __get_active_cart_id(self, user_id):
+        if not len(self.__get_active_cart(user_id)) > 0:
+            self.__create_cart(user_id)
+        return self.__get_active_cart(user_id)[0][0]
+
+    def __get_active_cart(self, user_id):
+        sql = (
+            "SELECT * FROM OrderHead WHERE customer_id = %s and status = 'in_progress'"
+        )
+        val = (user_id,)
+        self.cursor.execute(sql, val)
+        return self.cursor.fetchall()
+
+    def __create_cart(self, user_id):
+        sql = "INSERT INTO OrderHead (customer_id, status) VALUES (%s, 'in_progress')"
+        val = (user_id,)
+        self.cursor.execute(sql, val)
+        self.mydb.commit()
+
+    def __add_quant_cart_item(self, cart_id, item_id):
+        sql = "UPDATE ItemGroup SET quantity = quantity + 1 WHERE order_id = %s and item_id = %s"
+        val = (cart_id, item_id)
+
+        self.cursor.execute(sql, val)
+        self.mydb.commit()
+
+    def __add_item_to_cart(self, order_id, item_id):
+        sql = "INSERT INTO ItemGroup (order_id, item_id, quantity) VALUES (%s, %s, 1)"
+        val = (order_id, item_id)
+        self.cursor.execute(sql, val)
+        self.mydb.commit()
+
+    def __is_item_in_cart(self, user_id, item_id, order_id):
+        sql = "SELECT * FROM ItemGroup WHERE order_id = %s and item_id = %s and order_id = %s"
+        val = (user_id, item_id, order_id)
+        self.cursor.execute(sql, val)
+        return len(self.cursor.fetchall()) > 0
+
 
 if __name__ == "__main__":
     db = DB()
 
-    user_info = {
-        "username": "testP",
-        "first_name": "Pelle",
-        "last_name": "Olsson",
-        "password": "Pass",
-    }
+    cart = db.get_cart(1)
 
-    print(db.create_customer(user_info))
+    for x in cart:
+        print(x)
